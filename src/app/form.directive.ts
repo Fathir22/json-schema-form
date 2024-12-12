2d28087ea73ee0be8cae7fff7235caa695e69fd0
Arnaud GOURET <arnaud.gouret.e@thalesdigital.io>
2024-12-12 11:22:15 +0100

    Remove exemple

--- src/app/form.directive.ts
+++
@@ -1,8 +0,0 @@
-import { Directive, ViewContainerRef } from '@angular/core';
-
-@Directive({
-    selector: '[formHost]',
-})
-export class FormDirective {
-    constructor(public viewContainerRef: ViewContainerRef) { }
-}
